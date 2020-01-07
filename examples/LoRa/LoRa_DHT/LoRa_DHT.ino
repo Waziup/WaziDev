8ce24127e871468a56724d7df07468bcd25730a3
Corentin Dupont <corentin.dupont@gmail.com>
2020-01-07 19:13:17 +0100

    DHT

--- examples/LoRa/LoRa_DHT/LoRa_DHT.ino
+++ examples/LoRa/LoRa_DHT/LoRa_DHT.ino
@@ -4,22 +4,21 @@
  ********************/
  
 #include <WaziDev.h>
+#include <Utils.cpp>
 #include <DHT.h>
 
-//Constants
-#define DHTPIN 2     // what pin on the arduino is the DHT22 data line connected to
+
+#define DHTPIN 2        // what pin on the arduino is the DHT data line connected to
 #define DHTTYPE DHT11   // DHT 11
 
-// Initialize DHT sensor for normal Arduino
 DHT dht(DHTPIN, DHTTYPE);
 
 // new WaziDev with node address = 8 
-WaziDev *wazidev = new WaziDev("MyDevice", 8);
+WaziDev wazidev("MyDevice", 8);
 
 void setup() { // to run once
-  wazidev->setup();
+  wazidev.setup();
   Serial.println("DHT11 Humidity - Temperature Sensor");
-  Serial.println("RH\t Temp (C)");
 
 //  pinMode(5, OUTPUT);  digitalWrite(5, LOW);
   pinMode(3, OUTPUT);  digitalWrite(3, HIGH);
@@ -38,15 +37,15 @@ void loop() {
     Serial.println("Failed to read from DHT11 sensor!");
     return;
   }
-  //Send temperature as sensor "TC"
-  wazidev->sendSensorValue("TC", t);
-  //Send humidity as sensor "HUM"
-  wazidev->sendSensorValue("HUM", h);
 
-  Serial.print(h); 
-  Serial.print(" %\t\t");
-  Serial.print(t); 
-  Serial.println(" °C");
+  WaziDev::SensorVal fields[2] = {{"TC", t}, {"HUM", h}};
+  //Send temperature as sensor "TC"
+  wazidev.sendSensorValues(fields, 2);
+
+  char tf[10]; ftoa(tf, t, 2); 
+  char hf[10]; ftoa(hf, h, 2);
+  wazidev.writeSerial("%s %% %s °C\n", hf, tf);
+  Serial.flush();
   // Wait a few seconds between measurements. The DHT11 should not be read at a higher frequency of
   // about once every 2 seconds. So we add a 3 second delay to cover this.
   delay(3000);

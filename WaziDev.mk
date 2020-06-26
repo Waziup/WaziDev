565201a149757338a87707c08b1f461d15acb388
Corentin Dupont <corentin.dupont@gmail.com>
2020-06-26 17:37:44 +0200

    update Makefiles

--- WaziDev.mk
+++ WaziDev.mk
@@ -6,11 +6,6 @@ USER_LIB_PATH    = /home/cdupont/Documents/Waziup/WaziDev/libraries
 BOARD_TAG        = pro
 BOARD_SUB        = 8MHzatmega328 
 MONITOR_PORT     = /dev/ttyUSB0
-ARDUINO_LIBS     = SPI SX1272 LowPower EEPROM U8g2 Wire TinyGPSPlus OneWire DHT Dallas-Temperature WaziDev LiquidCrystal AES-128_V10 Base64 SoftwareSerial CayenneLPP ArduinoJson
+ARDUINO_LIBS     = SPI LowPower EEPROM Wire OneWire WaziDev Base64 SoftwareSerial ArduinoJson
 MONITOR_BAUDRATE = 38400 
 
-include /usr/share/arduino/Arduino.mk
-
-monitor:
-	stty -F $(MONITOR_PORT) raw $(MONITOR_BAUDRATE)
-	cat $(MONITOR_PORT)

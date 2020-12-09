9f36a303caa82526ae4f098e79fe2c855a40e149
Corentin Dupont <corentin.dupont@gmail.com>
2020-12-09 10:21:09 +0100

    actuation

--- examples/LoRa/LoRaWAN_LPP_LMIC/LoRaWAN_LPP_LMIC.ino
+++ examples/LoRa/LoRaWAN_LPP_LMIC/LoRaWAN_LPP_LMIC.ino
@@ -11,12 +11,13 @@
 #include <CayenneLPP.h>
 #include <Base64.h>
 
-// Enter here the Network session Key in Hex format. Example: 196AE5DFE9EBC9AB61F1EFCEA8E6D337
-static const PROGMEM u1_t NWKSKEY[16] = { 0x19, 0x6A, 0xE5, 0xDF, 0xE9, 0xEB, 0xC9, 0xAB, 0x61, 0xF1, 0xEF, 0xCE, 0xA8, 0xE6, 0xD3, 0x37};
-// Enter here the Network session Key in Hex format. Example: 93B9F0269BFB05610740EFEB8303D1E2
-static const u1_t PROGMEM APPSKEY[16] = { 0x93, 0xB9, 0xF0, 0x26, 0x9B, 0xFB, 0x05, 0x61, 0x07, 0x40, 0xEF, 0xEB, 0x83, 0x03, 0xD1, 0xE2 };
 // Enter here the Device address in Hex format. Example: 2601143F
 static const u4_t DEVADDR = 0x2601143F ; // <-- Change this address for every node!
+// Enter here the Network session Key in Hex format. Example: 196AE5DFE9EBC9AB61F1EFCEA8E6D337
+static const PROGMEM u1_t NWKSKEY[16] = { 0x19, 0x6A, 0xE5, 0xDF, 0xE9, 0xEB, 0xC9, 0xAB, 0x61, 0xF1, 0xEF, 0xCE, 0xA8, 0xE6, 0xD3, 0x37};
+// Enter here the Application session Key in Hex format. Example: 93B9F0269BFB05610740EFEB8303D1E2
+static const u1_t PROGMEM APPSKEY[16] = { 0x93, 0xB9, 0xF0, 0x26, 0x9B, 0xFB, 0x05, 0x61, 0x07, 0x40, 0xEF, 0xEB, 0x83, 0x03, 0xD1, 0xE2 };
+
 
 // These callbacks are only used in over-the-air activation, so they are
 // left empty here (we cannot leave them out completely unless
@@ -31,7 +32,7 @@ static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 10;
+const unsigned TX_INTERVAL = 0;
 
 // Pin mapping
 const lmic_pinmap lmic_pins = {
@@ -45,7 +46,7 @@ CayenneLPP lpp(10);
 
 // Define the single channel and data rate (SF) to use
 int channel = 0;
-int dr = DR_SF12;
+int dr = DR_SF9; //12
 
 // Disables all channels, except for the one defined above, and sets the
 // data rate (SF). This only affects uplinks; for downlinks the default
@@ -60,14 +61,13 @@ void forceTxSingleChannelDr() {
         }
     }
     // Set data rate (SF) and transmit power for uplink
-    LMIC_setDrTxpow(dr, 14);
+    LMIC_setDrTxpow(dr, 7); //12
 }
 
 void do_send(osjob_t* j){
-
     //Create payload
     lpp.reset();
-    lpp.addTemperature(1, 28.0);
+    lpp.addTemperature(1, 29.0);
     memcpy((char*)mydata, lpp.getBuffer(), lpp.getSize());
 
     // Check if there is not a current TX/RX job running
@@ -89,6 +89,20 @@ void onEvent (ev_t ev) {
        Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
        if (LMIC.txrxFlags & TXRX_ACK)
          Serial.println(F("Received ack"));
+        if (LMIC.dataLen) {
+          char data[LMIC.dataLen];
+          memcpy(data, LMIC.frame + LMIC.dataBeg, LMIC.dataLen);
+          char decoded[100];
+          base64_decode(decoded, data, sizeof(data));
+          Serial.println(decoded);
+
+          if(strcmp(decoded, "true") == 0) {
+            Serial.println(F("ON"));
+          } else {
+            Serial.println(F("OFF"));
+          }
+          
+        }
        // Schedule next transmission
        os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
     }

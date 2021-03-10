020502239d88e9bc427ff992720b3111d6160c2f
Corentin Dupont <corentin.dupont@gmail.com>
2021-03-10 16:35:13 +0100

    Actuation with JSON

--- examples/LoRaWAN/Actuation/Actuation.ino
+++ examples/LoRaWAN/Actuation/Actuation.ino
@@ -1,5 +1,6 @@
 #include <WaziDev.h>
 #include <xlpp.h>
+#include <Base64.h>
 
 // NwkSKey (Network Session Key) and Appkey (AppKey) are used for securing LoRaWAN transmissions.
 // You need to copy them from/to your LoRaWAN server or gateway.
@@ -12,14 +13,6 @@ unsigned char appSkey[16] = {0x23, 0x15, 0x8D, 0x3B, 0xBC, 0x31, 0xE6, 0xAF, 0x6
 
 // Copy'n'paste the key to your Wazigate: 23158D3BBC31E6AF670D195B5AED5525
 unsigned char nwkSkey[16] = {0x23, 0x15, 0x8D, 0x3B, 0xBC, 0x31, 0xE6, 0xAF, 0x67, 0x0D, 0x19, 0x5B, 0x5A, 0xED, 0x55, 0x25};
-
-// A valid payload for the downlink for this sketch has been generated like this:
-// > xlpp -e '{"colour0":"#ffaa00","switch1":true}'
-// < AIf/qgABjgE=
-// You can paste the base64 payload to Chirpstack.
-// On Windows, you might need to escape it like this:
-// > xlpp -e "{"""colour0""":"""#ffaa00""","""switch1""":true}"
-
 
 WaziDev wazidev;
 
@@ -36,7 +29,7 @@ void loop(void)
   // 1
   // Create xlpp payload.
   xlpp.reset();
-  xlpp.addTemperature(1, 20.3); // °C
+  xlpp.addTemperature(1, 45.7); // °C
 
   // 2.
   // Send paload with LoRaWAN.
@@ -77,55 +70,10 @@ void loop(void)
   serialPrintf("LoRaWAN frame size: %d\n", xlpp.offset+xlpp.len);
   serialPrintf("LoRaWAN payload len: %d\n", xlpp.len);
   serialPrintf("Payload: ");
-  printBase64(xlpp.getBuffer(), xlpp.len);
+  char payload[100];
+  base64_decode(payload, xlpp.getBuffer(), xlpp.len); 
+  serialPrintf(payload);
   serialPrintf("\n");
-
-  // 4.
-  // Read xlpp message.
-  // You must use the following pattern to properly parse xlpp payload.
-  int end = xlpp.len + xlpp.offset;
-  while (xlpp.offset < end)
-  {
-    // Always read the channel first ...
-    uint8_t chan = xlpp.getChannel();
-    serialPrintf("Chan %2d: ", chan);
-
-    // ... then the type ...
-    uint8_t type = xlpp.getType();
-
-    // ... then the value!
-    switch (type) {
-      case LPP_DIGITAL_OUTPUT:
-      {
-        uint8_t v = xlpp.getDigitalInput();
-        serialPrintf("Digital Output: %hu (0x%02x)\n", v, v);
-        break;
-      }
-      case LPP_ANALOG_OUTPUT:
-      {
-        float f = xlpp.getAnalogOutput();
-        serialPrintf("Analog Output: %.2f\n", f);
-        break;
-      }
-      case LPP_COLOUR:
-      {
-        Colour c = xlpp.getColour();
-        serialPrintf("Colour: R %d, G %d, B %d (#%02X%02X%02X)\n", c.r, c.g, c.b, c.r, c.g, c.b);
-        break;
-      }
-      case LPP_SWITCH:
-      {
-        uint8_t v = xlpp.getSwitch();
-        serialPrintf("Switch: %s\n", v?"on":"off");
-        break;
-      }
-      default:
-        // For all the other types, see https://github.com/Waziup/arduino-xlpp/blob/main/test/simple/main.cpp#L147
-        serialPrintf("Other unknown type.\n");
-        delay(60000);
-        return;
-    }
-  }
   
   delay(60000);
 }

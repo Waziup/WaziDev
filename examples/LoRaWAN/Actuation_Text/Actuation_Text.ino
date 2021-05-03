77d0ee9f3a790bf58f21596bf8b9052ae16dc1ec
Corentin Dupont <corentin.dupont@gmail.com>
2021-05-03 12:11:16 +0200

    bug

--- examples/LoRaWAN/Actuation_Text/Actuation_Text.ino
+++ examples/LoRaWAN/Actuation_Text/Actuation_Text.ino
@@ -3,9 +3,9 @@
 
 // LoRaWANKey is used as both NwkSKey (Network Session Key) and Appkey (AppKey) for secure LoRaWAN transmission.
 // Copy'n'paste the key to your Wazigate: 23158D3BBC31E6AF670D195B5AED5525
-unsigned char LoRaWANKey[16] = {0x23, 0x15, 0x8D, 0x3B, 0xBC, 0x31, 0xE6, 0xAF, 0x67, 0x0D, 0x19, 0x5B, 0x5A, 0xED, 0x55, 0x25};
+unsigned char LoRaWANKeys[16] = {0x23, 0x15, 0x8D, 0x3B, 0xBC, 0x31, 0xE6, 0xAF, 0x67, 0x0D, 0x19, 0x5B, 0x5A, 0xED, 0x55, 0x25};
 // Copy'n'paste the DevAddr (Device Address): 26011D87
-unsigned char DevAddr[4] = {0x26, 0x01, 0x1D, 0x88};
+unsigned char devAddr[4] = {0x26, 0x01, 0x1D, 0x88};
 // You can change the Key and DevAddr as you want.
 
 WaziDev wazidev;
@@ -13,7 +13,7 @@ WaziDev wazidev;
 void setup()
 {
     Serial.begin(38400);
-    wazidev.setupLoRaWAN(DevAddr, LoRaWANKey);
+    wazidev.setupLoRaWAN(devAddr, LoRaWANKeys);
 }
 
 XLPP xlpp(120);

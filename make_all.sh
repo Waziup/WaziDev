5c5b7c5aeff0b85a33379a2afaf167fed8d844a0
Corentin Dupont <corentin.dupont@gmail.com>
2021-05-04 11:14:59 +0200

    test

--- make_all.sh
+++ make_all.sh
@@ -1,17 +1,16 @@
 #Compile all examples and generate a result file
-INOS=`find ./examples -name "Makefile"`
+INOS=`find ./examples -name "*.ino"`
 BASE=$PWD
+ARDUINO_DIRECTORIES_USER=$PWD
+
 set +e #avoid that a single failure stops the script
 
 for f in $INOS
 do
         echo Compiling $f ...
-        DIR=`dirname $f`
-	cd $DIR
-        make
-        RES=$?
-        cd $BASE
-        echo "$f, $RES" >> test_results.csv
+        juLog arduino-cli compile -p /dev/ttyUSB0 --fqbn arduino:avr:pro $f 2>&1 > /dev/null
+        #RES=$?
+        #echo "$f, $RES, $ERR" >> test_results.csv
 done
 echo
 echo "##### FInished compiling #####"

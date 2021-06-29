748f5ebfddae62e9b511ba71cd5a8fabdc0d4028
moji <mojtaba.esk@gmail.com>
2021-06-29 16:45:23 +0200

    Soil moisture sensor added

--- examples/wazisense/wazisense.ino
+++ examples/wazisense/wazisense.ino
@@ -1,4 +1,3 @@
-
 #include <Wire.h>
 #include "i2c.h"
 
@@ -24,99 +23,152 @@ WaziDev wazidev;
 // Be careful: Do not connect a high voltage solar panel to the board i.e. Max: 6V
 #define SolarLevelPin A2
 
+/*-------*/
+
+// To save power we use a digital pin to power on the soil moisture sensor
+// whenever we need to read it
+#define SoilMoistureDataPin A6
+#define SoilMoisturePowerPin 6
+
+/*-------*/
+
+#define LEDPin 8
+
 /*-----------------------------*/
 
+// This function reads an analogue pin couple of times
+// and makes an average of it to have more accuracy
+unsigned int analogReadAvg(unsigned int pin)
+{
+#define TotalReads 10
+
+  unsigned int sum = 0;
+  for (int i = 0; i != TotalReads; i++)
+  {
+    delay(50);
+    sum += analogRead(pin);
+  }
+
+  return sum / TotalReads;
+}
+
+/*-----------------------------*/
 
 void setup()
 {
   Serial.begin(38400);
 
   /*-----------*/
-  
+
   unsigned char LoRaWANKey[16] = {0x23, 0x15, 0x8D, 0x3B, 0xBC, 0x31, 0xE6, 0xAF, 0x67, 0x0D, 0x19, 0x5B, 0x5A, 0xED, 0x55, 0x25}; // 23158D3BBC31E6AF670D195B5AED5525
-  unsigned char DevAddr[4] = {0x26, 0x01, 0x1D, 0x87}; // 26011D87
-  
+  unsigned char DevAddr[4] = {0x26, 0x01, 0x1D, 0x87};                                                                             // 26011D87
+
   /*-----------*/
 
-    uint8_t errSetup = wazidev.setupLoRaWAN(DevAddr, LoRaWANKey);
-    if (errSetup != 0)
+  uint8_t errSetup = wazidev.setupLoRaWAN(DevAddr, LoRaWANKey);
+  if (errSetup != 0)
+  {
+    serialPrintf("LoRaWAN Err %d\n", errSetup);
+    delay(60000);
+    return;
+  }
+
+  /*-----------*/
+
+  pinMode(LEDPin, OUTPUT);
+  pinMode(SoilMoisturePowerPin, OUTPUT);
+
+  /*-----------*/
+
+  serialPrintf("Probe SI7021: ");
+  if (si7021.initialize())
+  {
+    serialPrintf("SI7021 Sensor found!\n");
+  }
+  else
+  {
+
+    serialPrintf("SI7021 Sensor missing\n");
+    while (1)
     {
-      serialPrintf("LoRaWAN Err %d\n", errSetup);
-      delay(60000);
-      return;
-    }
+      delay(1 * 60 * 1000);
+    };
+  }
 
-    /*-----------*/
+  /*-----------*/
 
-    pinMode(8, OUTPUT);
+  for (int i = 0; i != 3; i++)
+  {
 
-    /*-----------*/
-      
-    serialPrintf("Probe SI7021: ");
-    if (si7021.initialize()) {
-      serialPrintf("Sensor found!\n");
-    
-    }else{
-      
-        serialPrintf("Sensor missing\n");
-        while(1) {delay(1*60*1000);};
-    }
-
-    /*-----------*/
-
-    for( int i = 0; i != 3; i++){
-      
-      digitalWrite(8, HIGH);delay(50);
-      digitalWrite(8, LOW); delay(50);
-      
-    }
-
+    digitalWrite(LEDPin, HIGH);
+    delay(50);
+    digitalWrite(LEDPin, LOW);
+    delay(50);
+  }
 }
 
-char payload[255];
+/*-----------------------------*/
 
 void loop(void)
-{ 
+{
 
-    serialPrintf("\n\n---------------------\n\n");
+  serialPrintf("\n\n---------------------\n");
 
-    digitalWrite(8, HIGH);delay(50);
-    digitalWrite(8, LOW); delay(50);
-    
-    /*----------*/
+  digitalWrite(LEDPin, HIGH);
+  delay(50);
+  digitalWrite(LEDPin, LOW);
+  delay(50);
 
-    static float humidity, temperature;
+  /*----------*/
 
-    si7021.getHumidity(humidity);
-    si7021.getTemperature(temperature);
-    si7021.triggerMeasurement();
+  float humidity, temperature;
 
-    serialPrintf("TEMP: %d.%02d \t HUMI: %d.%02d\n", (int)temperature, (int)(temperature*100)%100, (int)humidity, (int)(humidity*100)%100);
+  si7021.getHumidity(humidity);
+  si7021.getTemperature(temperature);
+  si7021.triggerMeasurement();
 
-    /*----------*/
+  serialPrintf("\nTemperature:\t\t%d.%02d \nRelative Humidity:\t%d.%02d", (int)temperature, (int)(temperature * 100) % 100, (int)humidity, (int)(humidity * 100) % 100);
 
-    static float solarLevel = analogRead(SolarLevelPin);
-    serialPrintf("Solar Level: \t %d\n", solarLevel);
+  /*----------*/
 
-    /*----------*/
-    
-    xlpp.reset();
-    xlpp.addRelativeHumidity(2, humidity);
-    xlpp.addTemperature(1, temperature);
+  unsigned int solarLevel = analogReadAvg(SolarLevelPin);
+  serialPrintf("\nSolar Level:\t\t%d", solarLevel);
 
-    xlpp.addAnalogInput(3, solarLevel);
-    
+  /*----------*/
 
-    serialPrintf("LoRaWAN send ... ");
-    int e = wazidev.sendLoRaWAN(xlpp.getBuffer(), xlpp.getSize());
-    if (e == 0){
-        serialPrintf("OK\n");
-        
-    }else{
-      
-        serialPrintf("Err %d\n", e);
-    }
-    
+  // Power on the sensor
+  digitalWrite(SoilMoisturePowerPin, HIGH);
+  delay(1000);
 
-    delay(30*60*1000); // wait before pushing, we need to use sleep mode for a better battery performance
+  unsigned int moistureLevel = analogReadAvg(SoilMoistureDataPin);
+  serialPrintf("\nMoisture Level:\t\t%d", moistureLevel);
+
+  // Power off the sensor
+  digitalWrite(SoilMoisturePowerPin, LOW);
+
+  /*----------*/
+
+  xlpp.reset();
+  xlpp.addRelativeHumidity(2, humidity);
+  xlpp.addTemperature(1, temperature);
+
+  xlpp.addAnalogInput(3, solarLevel);
+  xlpp.addAnalogInput(4, moistureLevel);
+
+  serialPrintf("\nLoRaWAN send ... ");
+  int e = wazidev.sendLoRaWAN(xlpp.getBuffer(), xlpp.getSize());
+  if (e == 0)
+  {
+    serialPrintf("\tOK");
+  }
+  else
+  {
+    serialPrintf("\nErr: %d", e);
+  }
+
+  serialPrintf("\nWaiting for 15 minutes... ");
+
+  // wait before pushing, we need to use sleep mode for a better battery performance
+  delay(900000); // 15 * 60 * 1000 : 15 minutes
+  serialPrintf("Done waiting");
 }

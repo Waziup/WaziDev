079746201247c137ec7dbd4f8a93ba7f9756f0c8
Corentin Dupont <corentin.dupont@gmail.com>
2021-09-16 10:38:55 +0200

    key bug

--- examples/LoRaWAN/DHT_RGB-LED/DHT_RGB-LED.ino
+++ examples/LoRaWAN/DHT_RGB-LED/DHT_RGB-LED.ino
@@ -14,9 +14,9 @@
 
 // LoRaWANKey is used as both NwkSKey (Network Session Key) and Appkey (AppKey) for secure LoRaWAN transmission.
 // Copy'n'paste the key to your Wazigate: 23158D3BBC31E6AF670D195B5AED5525
-unsigned char LoRaWANKey[16] = {0x23, 0x15, 0x8D, 0x3B, 0xBC, 0x31, 0xE6, 0xAF, 0x67, 0x0D, 0x19, 0x5B, 0x5A, 0xED, 0x55, 0x25};
+unsigned char loRaWANKey[16] = {0x23, 0x15, 0x8D, 0x3B, 0xBC, 0x31, 0xE6, 0xAF, 0x67, 0x0D, 0x19, 0x5B, 0x5A, 0xED, 0x55, 0x25};
 // Copy'n'paste the DevAddr (Device Address): 26011D89
-unsigned char DevAddr[4] = {0x26, 0x01, 0x1D, 0x89};
+unsigned char devAddr[4] = {0x26, 0x01, 0x1D, 0x89};
 // You can change the Key and DevAddr as you want.
 
 
@@ -36,7 +36,7 @@ DHT dht(DHT_PIN, DHT11);
 void setup()
 {
   Serial.begin(38400);
-  wazidev.setupLoRaWAN(DevAddr, LoRaWANKey);
+  wazidev.setupLoRaWAN(devAddr, loRaWANKey);
 
   pinMode(LED_RED_PIN, OUTPUT);
   pinMode(LED_GREEN_PIN, OUTPUT);
--- examples/LoRaWAN/Simple/Simple.ino
+++ examples/LoRaWAN/Simple/Simple.ino
@@ -3,9 +3,9 @@
 
 // LoRaWANKey is used as both NwkSKey (Network Session Key) and Appkey (AppKey) for secure LoRaWAN transmission.
 // Copy'n'paste the key to your Wazigate: 23158D3BBC31E6AF670D195B5AED5525
-unsigned char LoRaWANKey[16] = {0x23, 0x15, 0x8D, 0x3B, 0xBC, 0x31, 0xE6, 0xAF, 0x67, 0x0D, 0x19, 0x5B, 0x5A, 0xED, 0x55, 0x25};
+unsigned char loRaWANKey[16] = {0x23, 0x15, 0x8D, 0x3B, 0xBC, 0x31, 0xE6, 0xAF, 0x67, 0x0D, 0x19, 0x5B, 0x5A, 0xED, 0x55, 0x25};
 // Copy'n'paste the DevAddr (Device Address): 26011D87
-unsigned char DevAddr[4] = {0x26, 0x01, 0x1D, 0x87};
+unsigned char devAddr[4] = {0x26, 0x01, 0x1D, 0x87};
 // You can change the Key and DevAddr as you want.
 
 
@@ -16,7 +16,7 @@ void setup()
   Serial.begin(38400);
 
   serialPrintf("LoRaWAN setup ...\n");
-  uint8_t e = wazidev.setupLoRaWAN(DevAddr, LoRaWANKey);
+  uint8_t e = wazidev.setupLoRaWAN(devAddr, loRaWANKey);
   if (e != 0)
   {
     serialPrintf("Err %d\n", e);

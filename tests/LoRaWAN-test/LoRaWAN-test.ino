29fdc66e31d0cad11a539947fec2737c0bd04d46
corentin dupont <cdupont@cdupont-server>
2022-02-02 17:03:28 +0100

    python API

--- tests/LoRaWAN-test/LoRaWAN-test.ino
+++ tests/LoRaWAN-test/LoRaWAN-test.ino
@@ -3,12 +3,16 @@
 #include <Base64.h>
 #include <simpleRPC.h>
 
+// Copy'n'paste the DevAddr (Device Address): 26011D00
 unsigned char devAddr[4] = {0x26, 0x01, 0x1D, 0x22};
+// Copy'n'paste the key to your Wazigate: 23158D3BBC31E6AF670D195B5AED5525
 unsigned char appSkey[16] = {0x23, 0x15, 0x8D, 0x3B, 0xBC, 0x31, 0xE6, 0xAF, 0x67, 0x0D, 0x19, 0x5B, 0x5A, 0xED, 0x55, 0x25};
+// Copy'n'paste the key to your Wazigate: 23158D3BBC31E6AF670D195B5AED5525
 unsigned char nwkSkey[16] = {0x23, 0x15, 0x8D, 0x3B, 0xBC, 0x31, 0xE6, 0xAF, 0x67, 0x0D, 0x19, 0x5B, 0x5A, 0xED, 0x55, 0x25};
 
 WaziDev wazidev;
 XLPP xlpp(120);
+char payload[100] = "";
 
 void setup()
 {
@@ -16,24 +20,49 @@ void setup()
   wazidev.setupLoRaWAN(devAddr, appSkey, nwkSkey);
 }
 
-
 Object<uint8_t, char *> sendLoRaWAN(int temp)
 {
   xlpp.reset();
   xlpp.addTemperature(1, temp);
   uint8_t e = wazidev.sendLoRaWAN(xlpp.buf, xlpp.len);
 
-  char payload[100] = "";
-  e = wazidev.receiveLoRaWAN(xlpp.buf, &xlpp.offset, &xlpp.len, 6000);
+  e = wazidev.receiveLoRaWAN(xlpp.buf, &xlpp.offset, &xlpp.len, 3000);
   if (e == 0)
   {
     base64_decode(payload, xlpp.getBuffer(), xlpp.len);
+  } else {
+    payload[0] = '\0';
   }
   return {e, payload};
 }
 
+char *sendLoRaWAN2(int temp)
+{
+  char *res = malloc(50); 
+  xlpp.reset();
+  xlpp.addTemperature(1, temp);
+  uint8_t e = wazidev.sendLoRaWAN(xlpp.buf, xlpp.len);
+
+  e = wazidev.receiveLoRaWAN(xlpp.buf, &xlpp.offset, &xlpp.len, 1000);
+  switch (e)
+  {
+    case 0:
+      base64_decode(payload, xlpp.getBuffer(), xlpp.len);
+      sprintf(res, "Received: %s", payload);
+      break;
+    case 2: 
+      sprintf(res, "Error: Nothing received");
+      break;
+    default:
+      sprintf(res, "Error: %d", e);
+      break;
+  }
+  return res;
+}
+
 void loop(void)
 {
-  interface(Serial, sendLoRaWAN, "sendLoRaWAN: Send a LoRaWAN value. @value: int @return: string");
+  interface(Serial, sendLoRaWAN, "sendLoRaWAN: Send a LoRaWAN value. @value: int @return: string",
+                    sendLoRaWAN2, "sendLoRaWAN2: Send a LoRaWAN value. @value: int @return: string");
 }
 

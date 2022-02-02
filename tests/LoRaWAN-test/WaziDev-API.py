a9cce04015ab8497ac5de26ff81b993bb5e96957
corentin dupont <cdupont@cdupont-server>
2022-02-02 15:19:49 +0100

    python API

---
+++ tests/LoRaWAN-test/WaziDev-API.py
@@ -0,0 +1,28 @@
+
+from flask import Flask, request, jsonify
+from simple_rpc import Interface
+import os
+from threading import Thread
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+wazidev_port = os.getenv("WAZIDEV_PORT", '/dev/ttyUSB0')
+
+interface = Interface("/dev/ttyUSB0")
+
+
+@app.route('/', methods=['POST'])
+def postVal():
+   print(request.json)
+   print(request.json["val"])
+   return sendValueWaziDev(int(request.json["val"]))
+
+
+def sendValueWaziDev(val: int) -> str:
+    print("sending value: " + str(val))
+    return interface.sendLoRaWAN(val)
+
+app.run()
+
+if __name__ == '__main__':
+    print("main")

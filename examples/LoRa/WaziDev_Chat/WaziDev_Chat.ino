a9f1f742d57aea2ef1ab3a88fb81ab4cfc4993d9
Johann Forster <25847959+j-forster@users.noreply.github.com>
2025-01-28 12:05:20 +0100

    Streamline WaziDev Chat

--- examples/LoRa/WaziDev_Chat/WaziDev_Chat.ino
+++ examples/LoRa/WaziDev_Chat/WaziDev_Chat.ino
@@ -1,96 +1,101 @@
-#include <string.h>
 #include <WaziDev.h>
-
-// change your name here:
-const char* name = "Max";
 
 WaziDev wazidev;
 
 void setup() {
-  // open serial port, set data rate to 9600 bps
   Serial.begin(9600);
+  while (!Serial); // wait for serial port to connect
 
-  // setup LoRa radio chip
+  Serial.println("");
+  Serial.println("HTW LoRa Paramater");
+
+  // Activate SX1272 chipset
   wazidev.setupLoRa();
-  
-  Serial.print("Your name is: ");
-  Serial.print(name);
-  Serial.print("\r\n");
+
+  // Frequency
+  // 868.1 MHz, 868.3 MHz, 868.5 MHz, ..
+  wazidev.setLoRaFreq(868100000);
+
+  // Coding Rate
+  // LORA_CR_5 (4/5), LORA_CR_6 (4/6), LORA_CR_7 (4/7) or LORA_CR_8 (4/8)
+  wazidev.setLoRaCR(LORA_CR_5);
+
+  // Spreading factor
+  // LORA_SF_6 .. LORA_SF_12
+  wazidev.setLoRaSF(LORA_SF_12);
+
+  // Bandwidth
+  // LORA_BW_7_8 (7.8 kHz), .., LORA_BW_125 (125 kHz), LORA_BW_250 (250 kHz)
+  wazidev.setLoRaBW(LORA_BW_125);
+
+  // Sync Word
+  // 0x12 for LoRa (private networks), 0x34 for LoRaWAN (public networks)
+  wazidev.setLoRaSyncWord(0x12);
+
+  // Transmission Power [dbm]
+  // 0 .. 14 normal power, 20 high power
+  wazidev.setPowerDBM(14);
 }
 
+char buf[250];
+uint8_t len = 0;
 
-// The loop checks if there is data on the serial interface available.
-// If yes, it reads one line, and send a text message with lora like this:
-//   Name: hello world
-// After that, it waits 5 seconds for any incomming message.
 void loop() {
-  int e;
 
-  // buffer for sending and receiving
-  char buf[128];
-  uint8_t len;
-
-  // if serial data is available...
-  if (Serial.available() > 0)
+  while(Serial.available())
   {
-	// copy name to buffer
-    strcpy(buf, name);
-    len = strlen(name);
-    
-	// copy ": " to buffer
-    strcpy(buf+len, ": ");
-    len += 2;
-    
-    while (true)
+    // Read the serial (USB) data
+    buf[len] = Serial.read();   
+    if (buf[len] == '\n')
     {
-	  // read one line from serial
-      if(Serial.available() > 0)
-      {
-        char c = Serial.read();
-        Serial.print(c);
-        if (c == '\n' || c == 0)
-        {
-          buf[len]=0;
-          break;
-        }
-          
-        buf[len]=c;
-        len++;
-      }
+      buf[len] = NULL;
+
+      // Send LoRa Frame
+      Serial.println("");
+      Serial.println(">> Uplink >>");
+      Serial.print("Payload: \""); Serial.print(buf); Serial.print("\" ");
+      Serial.print(len); Serial.println(" B");
+
+      long startTime = millis();
+      wazidev.sendLoRa(buf, len, false);
+      long endTime = millis();
+
+      Serial.print("Time On Air: "); Serial.print(endTime-startTime); Serial.println(" ms");
+
+      len = 0;
     }
-	
-	// 1.
-	// Send LoRa message
-
-    serialPrintf(buf);
-    serialPrintf("\r\nSending %d bytes ... ", len);
-    e = wazidev.sendLoRa(buf, len, false);
-    if (e)
-      serialPrintf("Err %d\r\n", e);
     else
-      serialPrintf("OK\r\n");
+    {
+      len ++;
+    }
   }
-  
-  // 2.
-  // Receive LoRa message
-  // (waiting for 5 seconds only)
 
-  serialPrintf("Receiving ... ");
-  e = wazidev.receiveLoRa(buf, &len, 5000, false);
-  if (e)
+  // Receive LoRa Frame
+  uint8_t err = wazidev.receiveLoRa(buf, &len, 0, false);
+  switch(err)
   {
-    if (e == ERR_LORA_TIMEOUT)
-      serialPrintf("nothing received\r\n");
-    else 
-      serialPrintf("Err %d\r\n", e);
-  }
-  else
-  {
-    serialPrintf("OK\r\n");
-    buf[len+1]=0;
-    serialPrintf(buf);
-    serialPrintf("\r\n");
-  }
-  
+    case 0: // ok
 
+      Serial.println("");
+      Serial.println("<< Downlink <<");
+      buf[len] = NULL; // terminated with NULL like a C string
+      Serial.print("Payload: \""); Serial.print(buf); Serial.print("\" ");
+      Serial.print(len); Serial.println(" B");
+      // Signal-to-Noise Ratio (SNR), -20dB (noisy, bad) .. +10db (clear, good)
+      Serial.print("SNR: "); Serial.print(wazidev.loRaSNR); Serial.println("dB");
+      // Received Signal Strength Indication (RSSI), -120dBm (weak) .. -30dBm (strong)
+      Serial.print("RSSI: "); Serial.print(wazidev.loRaRSSI); Serial.println("dBm");
+      break;
+
+    case ERR_LORA_TIMEOUT:
+      // Nothing received
+      break;
+
+    default: // err
+      Serial.print("Error: "); Serial.println(err);
+      break; 
+  }
+
+  delay(1000);
+  Serial.print(".");
 }
